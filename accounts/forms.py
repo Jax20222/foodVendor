1a493262db60339e27bc9bff457a314beb50ca92
Jaime Patino <drjaimep365@gmail.com>
2022-10-22 15:40:36 -0500

    Mejorando fields de address
    Vamos mejorando el field address y preparandolo para conectarlo con los servicios de google

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,7 @@
 from socket import fromshare
 from django import forms
-from .models import User
+from .models import User, UserProfile
+from .validators import allow_only_images_validator
 
 
 class UserForm(forms.ModelForm):
@@ -19,3 +20,23 @@ class UserForm(forms.ModelForm):
             raise forms.ValidationError(
                 "Password does not match"
             )
+
+class UserProfileForm(forms.ModelForm):
+    address = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Start typing...', 'required': 'required'}))
+    profile_picture = forms.FileField(widget=forms.FileInput(attrs={'class':'btn btn-info'}), validators=[allow_only_images_validator])
+    cover_photo = forms.FileField(widget=forms.FileInput(attrs={'class':'btn btn-info'}), validators=[allow_only_images_validator])
+    
+    #altitude = forms.CharField(widget=forms.TextInput(attrs={'readonly':'readonly'}))
+    #longitude = forms.CharField(widget=forms.TextInput(attrs={'readonly':'readonly'}))
+    
+    class Meta:
+        model = UserProfile
+        fields = ['profile_picture', 'cover_photo', 'address', 'country', 'state', 'city', 'zip_code', 'altitude', 'longitude']
+          
+    def __init__(self, *args, **kargs):
+        super(UserProfileForm, self).__init__(*args, **kargs)
+        for field in self.fields:
+            if field == 'altitude' or field == 'longitude':
+                self.fields[field].widget.attrs['readonly'] = 'readonly'
+
+        
